015aa8002d9753325df90a6ed7a03a4b0d17abad
clara jarvis <claraejarvis@gmail.com>
2025-02-28 10:24:57 -0500

    Create altspell.h

---
+++ altspell.h
@@ -0,0 +1,20 @@
+// Copyright 2025 Clara Jarvis. Released under the MIT license.
+
+
+
+#ifndef __ISO646_H
+#define __ISO646_H
+
+#define and &&
+#define and_eq &=
+#define bitand &
+#define bitor |
+#define compl ~
+#define not !
+#define not_eq !=
+#define or ||
+#define or_eq |=
+#define xor ^
+#define xor_eq ^=
+
+#endif
